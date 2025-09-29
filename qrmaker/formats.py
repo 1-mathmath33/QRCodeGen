90b04a4205a97f0a83e148db6edcf9cf737ade42
Yash Pratap Singh <yashuthakur2004@gmail.com>
2025-09-29 20:52:29 +0530

    Create encoder for email

--- qrmaker/formats.py
+++ qrmaker/formats.py
@@ -16,3 +16,17 @@ def encode_url(data: dict):
         value = "https://" + value
 
     return value
+
+@register_encoder("email")
+def encode_email(data: dict):
+    """
+    Input: dictionary with "email" key
+    expects an email key and value pair
+    raise ValueError if key is not email or the value is empty
+    A valid input is to be given
+    """
+    value=data.get("email", "").strip()
+    if not value:
+        raise ValueError("Missing email key or value")
+    value ="mailto:" + str(value) # can this be improved??
+    return value
